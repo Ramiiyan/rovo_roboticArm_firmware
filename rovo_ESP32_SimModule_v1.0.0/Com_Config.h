14c1921b8cff59737270c4ba2a5abe1e432c36e2
Ramiiyan <sriraguhan.2017380@iit.ac.lk>
2022-11-27 12:35:18 +0530

    esp32 sim module v1 added.

---
+++ rovo_ESP32_SimModule_v1.0.0/Com_Config.h
@@ -0,0 +1,104 @@
+#define TINY_GSM_MODEM_SIM7000
+#include <Arduino.h>
+#include <MQTT.h>
+#include <TinyGsmClient.h>
+
+#include "ServoMotor.h"
+
+#define PUB_TOPIC           {{mqtt_setting.pub_topic}}      // "tester2/tesingdev2/v3/common"
+#define SUB_TOPIC           {{mqtt_setting.sub_topic}}      // "sub/1119/tester2/tesingdev2/v3/pub"
+
+#define MQTT_USERNAME       {{mqtt_setting.mqtt_username}}  // "tester2-tesingdev2-v3_3220"
+#define MQTT_PASSWORD       {{mqtt_setting.mqtt_password}}  // "1572435365_3220"
+#define MQTT_HOST           {{mqtt_setting.mqtt_host}}      // "mqtt.iot.ideamart.io"
+#define MQTT_PORT           {{mqtt_setting.mqtt_port}}      // 1883
+
+#define BAUD_RATE           {{serial_setting.baud_rate}}          // 115200
+#define SIM_BAUD_RATE       {{serial_setting.sim_module}}         // 9600
+#define TXD                 {{serial_setting.sim_module_Txd}}     // 16    //TXD
+#define RXD                 {{serial_setting.sim_module_Rxd}}     // 17    //RXD
+
+#define GSM_APN             "dialogbb"
+#define NB_APN              "nbiot"
+#define NETWORK_MODE        13
+
+//#define ideaBoard_PWRKEY    13
+//#define ideaBoard_RX        8
+//#define ideaBoard_TX        7
+//#define ideaBoard_RST       11
+
+HardwareSerial SerialSIM(2);
+TinyGsm modem(SerialSIM);
+TinyGsmClient client(modem);
+MQTTClient mqtt;
+
+String clientId = "";
+String getmsg;
+char setmsg[150];
+
+String control_mode = "{\"action\":\"control\",\"param\":{\"mac\":\"1119\"}}";
+String monitor_mode = "{\"action\":\"monitor\",\"param\":{\"mac\":\"1119\"}}";
+String cpsi;
+boolean ConnectToMQTT();
+void sendRAPosition(int b, int j1, int j2, int j3, int j4, int fe);
+//void CallBack(char *t, byte *payload,unsigned int l);
+void messageReceived(String &topic, String &payload);
+
+String getCPSI();
+byte action = 0;
+//byte action_value(byte action_val); // publish mode = 0 (default) , subcribe mode = 1 return 0/1
+
+boolean ConnectToMQTT(){
+  randomSeed(analogRead(39)); //analog pin 5 used to genarate random number
+  clientId = "TEST-" + String(millis()) + String(random(0, 100000000), HEX);
+    Serial.println(clientId);
+  if (!mqtt.connect(clientId.c_str(), MQTT_USERNAME, MQTT_PASSWORD)){
+    Serial.println("MQTT fail");
+    return false;
+  }
+  Serial.println(SUB_TOPIC);
+  mqtt.subscribe(SUB_TOPIC);
+  return mqtt.connected();
+}
+
+void messageReceived(String &topic, String &payload) {
+//  Serial.println("incoming: " + topic + " - " + payload);
+  
+  if (control_mode == payload){
+    action = 1;
+    Serial.println(F("Control_Mode Activated"));
+    
+  }else if (monitor_mode == payload){
+    action = 0;
+    Serial.println(F("Monitor_Mode Activated"));
+    
+  }else{
+    getmsg = payload;
+//    Serial.print("header :");
+//    Serial.println(getmsg);
+  }
+}
+  
+
+void sendRAPosition(int b, int j1, int j2, int j3, int j4, int fe){
+//  {"eventName":"Rovo","status":"<none>","b":"<b>","j1":"<j1>","j2":"<j2>","j3":"<j3>","j4":"<j4>","fe":"<fe>","param":{"mac":"<macAddressOfDevice>"}}
+//  sprintf(setmsg,"{\"eventName\":\"Rovo\",\"b\":\"%d\",\"j1\":\"%d\",\"j2\":\"%d\",\"j3\":\"%d\",\"j4\":\"%d\",\"fe\":\"%d\",\"param\":{\"mac\":\"1119\"}}",10,11,12,13,14,15);
+  sprintf(setmsg,"{\"b\":\"%d\",\"j1\":\"%d\",\"j2\":\"%d\",\"j3\":\"%d\",\"j4\":\"%d\",\"fe\":\"%d\",\"mac\":\"1119\"}",b,j1,j2,j3,j4,fe);
+  if (!mqtt.publish(PUB_TOPIC, setmsg)){
+    Serial.println(F("Failed."));
+  }else{
+    Serial.print(setmsg);
+    Serial.println(F("SEND:OK"));
+  }
+  //delay(1000);
+}
+
+String getCPSI(){
+  SerialSIM.println("AT+CPSI?");
+  delay(500);
+  while (SerialSIM.available()){
+    cpsi = SerialSIM.readString();
+  }
+  delay(500);
+  return cpsi;
+}
---
+++ rovo_ESP32_SimModule_v1.0.0/ServoReadHardwareTrial_ESP32_multiservo_SimModule.ino
@@ -0,0 +1,132 @@
+#include "ServoMotor.h" // custom library header for Servo motors
+#include "Com_Config.h" // custom library header for GSM_Module.
+#include "mainTemplate.h"
+
+/*ServoMotor  *myServo1, *myServo2; */
+ServoMotor *servo_list[SERVO_COUNT];
+boolean hasInitControl = false;
+boolean hasInitMonitor = false;
+int servoPos[SERVO_COUNT]; //using in publish positions (monitor)
+
+int getPos[SERVO_COUNT]; //using in control
+char *posName[] ={"b","j1","j2","fe","j3","j4"};
+String recentCmsg = "hello";
+void setup() {
+  Serial.begin(BAUD_RATE);
+  delay(10);
+  Serial.println("setup begin...");
+  
+  SerialSIM.begin(SIM_BAUD_RATE, SERIAL_8N1, (int8_t) RXD, (int8_t) TXD);
+  modem.setNetworkMode(NETWORK_MODE); // 38-nbiot 13-gsm
+//  modem.setPreferredMode(2);  // only for nbiot
+  modem.getModemName();
+  Serial.println(modem.getModemInfo());
+  Serial.println(modem.getIMEI());
+  modem.restart();
+  delay(2000);
+  Serial.println(F("Finding Network..."));
+  if (!modem.waitForNetwork()) {
+    Serial.println(F("Network Fail"));
+    while (true);
+  }else {
+    Serial.println(F("Network identified."));
+    Serial.print(F("Signal Strength : "));
+    Serial.println(modem.getSignalQuality());
+    
+    if (!modem.gprsConnect(GSM_APN, "", "")) {
+      Serial.println(F("GPRS Fail"));
+    }else {
+      Serial.println(F("GPRS Connected."));
+      Serial.println(getCPSI());
+      mqtt.begin(MQTT_HOST, MQTT_PORT,client);
+      mqtt.onMessage(messageReceived);
+    }
+  }
+
+  /*Servo Intializing */ 
+  for(int i=0; i<SERVO_COUNT; i++ ){
+    Serial.print(F("Servo Motor - "));
+    Serial.println(i+1);
+    servo_list[i] = new ServoMotor(servo_DPins[i],servo_APins[i]);
+    
+//    servo_list[i]->calibrate_Range(0,180);
+    servo_list[i]->calibrate_Range(servo_range[i][0],servo_range[i][1]); //[0]- min, [1]- max
+    delay(100);
+  }
+}
+
+void loop() {
+  if (mqtt.connected()) {
+    mqtt.loop();
+  }else{
+    Serial.println(F("Connecting MQTT..."));
+    ConnectToMQTT();
+    delay(100);
+  }
+  delay(1000);
+  
+  if(action == 0){  // publish mode (monitor) (transmit current position to Platform)
+    hasInitControl = false;
+    if(!hasInitMonitor){
+      for(int i=0; i<SERVO_COUNT; i++ ){
+        Serial.print(F("Servo Motor Monitor mode- "));
+        Serial.println(i+1);
+        servo_list[i]->switch_To_monitor();
+        delay(100);
+      }
+      hasInitMonitor = true;
+    }
+    for(int i=0; i<SERVO_COUNT; i++){
+      Serial.print("myServo ");
+      Serial.print(i+1);
+      Serial.print(" Pos : ");
+      servoPos[i] = (int)servo_list[i]->cur_Pos();
+      Serial.println(servoPos[i]);
+    }
+    
+    if(SERVO_COUNT == 2){
+      sendRAPosition(servoPos[0],servoPos[1],0,0,0,0);
+    }else if(SERVO_COUNT == 4){
+      sendRAPosition(servoPos[0],servoPos[1],servoPos[2],0,0,servoPos[3]); //b,j1,j2,fe
+    }else if (SERVO_COUNT == 6){
+      sendRAPosition(servoPos[0],servoPos[1],servoPos[2],servoPos[3],servoPos[4],servoPos[5]); //b,j1,j2,j3,j4,fe
+    }
+    delay(400);
+    
+  }
+  if (action == 1){  // subscribe mode (control)(receive positions from platform)
+    hasInitMonitor = true;
+    if(!hasInitControl){
+      for(int i=0; i<SERVO_COUNT; i++ ){
+        Serial.print(F("Servo Motor Control mode- "));
+        Serial.println(i+1);
+        servo_list[i]->switch_To_control();
+      }
+      hasInitControl = true;
+    }
+    if(getmsg != recentCmsg){
+      
+      Serial.print(F("Recieved event :"));
+      Serial.println(getmsg);
+  
+      deserializeJson(doc, getmsg);
+      JsonObject obj = doc.as<JsonObject>();
+      
+      for(int i=0; i<SERVO_COUNT; i++ ){
+        getPos[i] = obj[String(posName[i])];
+        Serial.println(getPos[i]);
+        servo_list[i]->control(getPos[i]);
+      }
+      Serial.println("*************");
+      recentCmsg = getmsg;
+    }
+         
+//    if(prePos != curPos){
+//    Serial.print(curPos);
+      
+//    }
+  }
+
+  
+ 
+}
